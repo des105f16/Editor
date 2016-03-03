2aa93ac2d601595d4c9675956015d8891029db60
deaddog <mikkelslsl@gmail.com>
2016-03-03 11:58:23 +0100

    Set up the project to start a simple editor for the new language.

--- DLM.Editor/Program.cs
+++ DLM.Editor/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SablePP.Tools.Editor;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,11 @@ namespace DLM.Editor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            var editor = new SimpleEditor();
+            editor.CodeTextBox.Executer = new CompilerExecuter();
+
+            Application.Run(editor);
         }
     }
 }
