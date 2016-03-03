2d9e3e0fee1156e5fd219bbb92f106cfe16de745
deaddog <mikkelslsl@gmail.com>
2016-03-03 13:16:41 +0100

    Added a simple shell for generating C code and running MinGW gcc.

---
+++ DLM.Editor/CGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SablePP.Tools.Nodes;
+using SablePP.Tools.Generate;
+
+namespace DLM.Editor
+{
+    public class CGenerator : Analysis.DepthFirstAdapter
+    {
+        private PatchElement element;
+
+        private CGenerator()
+        {
+            this.element = new PatchElement();
+        }
+
+        public static string GenerateC(Node node)
+        {
+            CGenerator gen = new CGenerator();
+
+            return gen.Visit((dynamic)node);
+        }
+        
+        protected override void HandleDefault(Node node)
+        {
+            if (node is Token)
+                element.Emit((node as Token).Text);
+            else
+                throw new NotImplementedException();
+        }
+    }
+}
---
+++ DLM.Editor/CompilerExecuter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DLM.Editor.Nodes;
+using SablePP.Tools;
+using SablePP.Tools.Nodes;
+using SablePP.Tools.Error;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace DLM.Editor
+{
+    public partial class CompilerExecuter
+    {
+        public override void Validate(Start<PExpression> root, CompilationOptions compilationOptions)
+        {
+            CompileWithGCC(root, compilationOptions.ErrorManager);
+            
+            base.Validate(root, compilationOptions);
+        }
+        
+        private static void CompileWithGCC(Node root, ErrorManager errors)
+        {
+            var file = Path.GetTempFileName();
+            File.WriteAllText(file, CGenerator.GenerateC(root));
+
+            ProcessStartInfo psi = new ProcessStartInfo(@"C:\MinGW\bin\gcc.exe", "-x c \"" + file + "\"")
+            {
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+                UseShellExecute = false
+            };
+
+            psi.CreateNoWindow = true;
+
+            string[] lines;
+
+            using (var p = Process.Start(psi))
+            {
+                p.WaitForExit();
+                lines = p.StandardError.ReadToEnd().Split('\r', '\n');
+            }
+
+            foreach (var l in lines)
+            {
+                var m = Regex.Match(l, @"\:(?<line>[0-9]+)\:(?<pos>[0-9]+)\:(?<message>[^\n\r]+)");
+                if (m.Success)
+                {
+                    int line = int.Parse(m.Groups["line"].Value);
+                    int pos = int.Parse(m.Groups["pos"].Value);
+                    string message = m.Groups["message"].Value.Trim();
+
+                    ErrorType type = ErrorType.Error;
+
+                    var typeStr = Regex.Match(message, "(?<type>error): (?<message>[^\n\r]+)");
+                    if (typeStr.Success)
+                    {
+                        type = ErrorType.Error;
+                        message = typeStr.Groups["message"].Value.Trim();
+                    }
+
+                    CompilerError e = new CompilerError(ErrorType.Error, new Position(line, pos), new Position(line, pos), "GCC; " + message);
+                    errors.Register(e);
+                }
+            }
+        }
+    }
+}
