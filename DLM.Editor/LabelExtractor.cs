88bcd8911da579555fdb8c1c29fb947e7580cb37
deaddog <mikkelslsl@gmail.com>
2016-03-06 13:34:25 +0100

    The label extractor creates policy labels for principal policies.

--- DLM.Editor/LabelExtractor.cs
+++ DLM.Editor/LabelExtractor.cs
@@ -32,5 +32,57 @@ namespace DLM.Editor
             if (errorManager.Errors.Count > 0)
                 return;
         }
+
+        protected override void HandleAPrincipalPolicy(APrincipalPolicy node)
+        {
+            Label lbl = null;
+            if (node.Owner is ALowerPrincipal)
+            {
+                if (node.Readers.Count != 1 || !(node.Readers[0] is AUpperPrincipal))
+                    errorManager.Register(node, "A policy with no owners must specify * for its readers.");
+                else
+                    lbl = Label.LowerBound;
+            }
+            else if (node.Owner is AUpperPrincipal)
+            {
+                if (node.Readers.Count != 1 || !(node.Readers[0] is ALowerPrincipal))
+                    errorManager.Register(node, "A policy with all principals as owners must specify _ for its readers.");
+                else
+                    lbl = Label.UpperBound;
+            }
+            else
+            {
+                var ownerName = (node.Owner as APrincipal).Identifier.Text;
+                Principal owner;
+
+                if (!principals.TryGetValue(ownerName, out owner))
+                    errorManager.Register(node.Owner, $"Use of undeclared principal {ownerName}.");
+
+                List<Principal> readers = new List<Principal>();
+                foreach (var reader in node.Readers)
+                {
+                    if (reader is ALowerPrincipal)
+                        errorManager.Register(reader, "Only a * owner can specify the _ reader-set.");
+                    else if (reader is AUpperPrincipal)
+                        errorManager.Register(reader, "Only a _ owner can specify the * reader-set.");
+                    else
+                    {
+                        string name = (reader as APrincipal).Identifier.Text;
+                        Principal r;
+
+                        if (!principals.TryGetValue(name, out r))
+                            errorManager.Register(node.Owner, $"Use of undeclared principal {name}.");
+                        else
+                            readers.Add(r);
+                    }
+                }
+
+                if (owner != null)
+                    lbl = new PolicyLabel(
+                        new Policy(owner, readers));
+            }
+
+            Output[node] = lbl;
+        }
     }
 }
