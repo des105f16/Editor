2672b1f114f3c2c4cbd81fefe9f6dc58c6118edb
deaddog <mikkelslsl@gmail.com>
2016-03-06 01:20:57 +0100

    Added a partial definition for the type production, with a Label property.

---
+++ DLM.Editor/Nodes/PType.cs
@@ -0,0 +1,15 @@
+﻿using DLM.Inference;
+
+namespace DLM.Editor.Nodes
+{
+    public partial class PType
+    {
+        private Label label;
+
+        public Label DeclaredLabel
+        {
+            get { return label; }
+            set { label = value; }
+        }
+    }
+}
