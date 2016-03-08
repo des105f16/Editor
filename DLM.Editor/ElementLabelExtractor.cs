de8a5606096e4361523cc7bb2189f25b783d08f8
mikaelec <mikaelec@gmail.com>
2016-03-08 12:48:57 +0100

    Added empty ElementLabelExtractor visitor.

---
+++ DLM.Editor/ElementLabelExtractor.cs
@@ -0,0 +1,20 @@
+﻿using DLM.Editor.Analysis;
+using SablePP.Tools.Error;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLM.Editor
+{
+    public class ElementLabelExtractor : DepthFirstAdapter
+    {
+        private ErrorManager errorManager;
+
+        public ElementLabelExtractor(ErrorManager errorManager)
+        {
+            this.errorManager = errorManager;
+        }
+    }
+}
