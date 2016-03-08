f17a2af4f1810744c0d74afc06cefee2856530fb
deaddog <mikkelslsl@gmail.com>
2016-03-08 13:44:42 +0100

    Created a partial definition for PLabel where its actual label is set.

---
+++ DLM.Editor/Nodes/PLabel.cs
@@ -0,0 +1,15 @@
+﻿using DLM.Inference;
+
+namespace DLM.Editor.Nodes
+{
+    public partial class PLabel
+    {
+        private Label labelValue;
+
+        public Label LabelValue
+        {
+            get { return labelValue; }
+            set { labelValue = value; }
+        }
+    }
+}
