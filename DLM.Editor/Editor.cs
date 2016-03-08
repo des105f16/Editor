e8088ad7fec3152f3bb95a2414edab41d36420e4
deaddog <mikkelslsl@gmail.com>
2016-03-08 15:00:05 +0100

    Removed the ListForm.

--- DLM.Editor/Editor.cs
+++ DLM.Editor/Editor.cs
@@ -12,32 +12,16 @@ namespace DLM.Editor
 {
     public partial class Editor : SablePP.Tools.Editor.EditorForm
     {
-        private ListForm list;
-
         public Editor()
         {
             InitializeComponent();
 
-            list = new ListForm();
-            FormClosing += (s, e) =>
-            {
-                list.CanClose = true;
-                list.Close();
-            };
-
             codeTextBox1.CompilationCompleted += CodeTextBox_CompilationCompleted;
-        }
-        protected override void OnLoad(EventArgs e)
-        {
-            base.OnLoad(e);
-            list.Show();
         }
 
         private void CodeTextBox_CompilationCompleted(object sender, EventArgs e)
         {
             var comp = codeTextBox1.Executer as CompilerExecuter;
-
-            list.Set(comp.Result);
         }
     }
 }
