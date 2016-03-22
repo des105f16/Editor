ebcf604d0a6433a9fc7cbf1dff0068856e379887
mikaelec <mikaelec@gmail.com>
2016-03-22 14:18:56 +0100

    Added principals for declared functions, with appropriate check when adding ordinary principals.

--- DLM.Editor/PrincipalExtractor.cs
+++ DLM.Editor/PrincipalExtractor.cs
@@ -26,19 +26,30 @@ namespace DLM.Editor
 
         protected override void HandlePRoot(PRoot node)
         {
+            foreach (var f in node.Statements.OfType<AFunctionDeclarationStatement>())
+            {
+                string name = f.Identifier.Text;
+                principals.Add(name, new FunctionPrincipal(name));
+            }
 
             foreach (var pd in node.PrincipalDeclarations)
             {
                 foreach (var p in pd.Principals)
                 {
                     string name = p.Identifier.Text;
-                    if (principals.ContainsKey(name))
-                        errorManager.Register(p, $"The principal {name} has already been defined.");
+                    Principal principal;
+                    if (principals.TryGetValue(name, out principal))
+                    {
+                        if (principal is FunctionPrincipal)
+                            errorManager.Register(p, $"The principal {principal.Name} is already defined through a function of the same name.");
+                        else
+                            errorManager.Register(p, $"The principal {principal.Name} has already been defined.");
+                    }
                     else
                         principals.Add(name, new Principal(name));
                 }
             }
-
+            
             Visit(node.PrincipalHierarchyStatements);
         }
 
@@ -80,6 +91,11 @@ namespace DLM.Editor
                 else
                     dominant.AddSubordinate(subordinate);
             }
+        }
+
+        private class FunctionPrincipal : Principal
+        {
+            public FunctionPrincipal(string name) : base(name) { }
         }
     }
 }
