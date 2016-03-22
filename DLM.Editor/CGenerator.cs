b2599366ac3c16bff1ec0fcc5103f10ecac3ca32
deaddog <mikkelslsl@gmail.com>
2016-03-22 09:37:47 +0100

    Included simple treewalkers for getting the first/last tokens from nodes.

--- DLM.Editor/CGenerator.cs
+++ DLM.Editor/CGenerator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using SablePP.Tools.Nodes;
 using DLM.Editor.Nodes;
 using System.Collections.Generic;
@@ -23,5 +22,66 @@ namespace DLM.Editor
 
             return gen.text.ToString();
         }
+
+        #region Token Locater classes
+
+        private class FirstToken : SablePP.Tools.Analysis.DepthFirstTreeWalker
+        {
+            private Token token = null;
+
+            public static Token Find(Node node)
+            {
+                var finder = new FirstToken();
+                finder.Visit(node);
+                return finder.token;
+            }
+            public static Token Find(IEnumerable<Node> nodes)
+            {
+                return Find(nodes.First());
+            }
+
+            private FirstToken() { }
+
+            public override void Visit(Production production)
+            {
+                if (this.token == null)
+                    base.Visit(production);
+            }
+            public override void Visit(Token token)
+            {
+                if (this.token == null)
+                    this.token = token;
+            }
+        }
+        private class LastToken : SablePP.Tools.Analysis.DepthFirstReversedTreeWalker
+        {
+            private Token token = null;
+
+            public static Token Find(Node node)
+            {
+                var finder = new LastToken();
+                finder.Visit(node);
+                return finder.token;
+            }
+            public static Token Find(IEnumerable<Node> nodes)
+            {
+                return Find(nodes.Last());
+            }
+
+            private LastToken() { }
+
+            public override void Visit(Production production)
+            {
+                if (this.token == null)
+                    base.Visit(production);
+            }
+            public override void Visit(Token token)
+            {
+                if (this.token == null)
+                    this.token = token;
+            }
+        }
+
+        #endregion
     }
 }
