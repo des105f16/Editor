d00d662ccaafc89da5bc2be07b515f1a8c1a485f
deaddog <mikkelslsl@gmail.com>
2016-03-24 14:59:27 +0100

    Created a simple wpf project.

---
+++ DLM.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using SablePP.Tools.Editor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms.Integration;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DLM.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var codeTextBox = new CodeTextBox() { Executer = new DLM.Compiler.CompilerExecuter() };
+
+            Loaded += (s, e) => codegrid.Child = new WindowsFormsHost() { Child = codeTextBox };
+        }
+    }
+}
