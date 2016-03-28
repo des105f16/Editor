a49f6aef1813cf6bc0862300a1c56ad2fceeeb22
deaddog <mikkelslsl@gmail.com>
2016-03-28 21:38:12 +0200

    Removed the FunctionDeclaration class. The data is already available in the ast.

--- DLM.Compiler/LabelInferer.cs
+++ DLM.Compiler/LabelInferer.cs
@@ -16,7 +16,7 @@ namespace DLM.Compiler
 
         private ErrorManager errorManager;
         private ScopedDictionary<string, PType> types;
-        private Dictionary<string, FunctionDeclaration> functionLabels;
+        private Dictionary<string, AFunctionDeclarationStatement> functionLabels;
 
         private LabelStack authority;
         private LabelStack basicBlock;
@@ -47,7 +47,7 @@ namespace DLM.Compiler
 
             this.errorManager = errorManager;
             types = new ScopedDictionary<string, PType>();
-            functionLabels = new Dictionary<string, FunctionDeclaration>();
+            functionLabels = new Dictionary<string, AFunctionDeclarationStatement>();
 
             authority = new LabelStack(true);
             basicBlock = new LabelStack(true);
@@ -64,7 +64,7 @@ namespace DLM.Compiler
         protected override void HandleAFunctionDeclarationStatement(AFunctionDeclarationStatement node)
         {
             var fName = node.Identifier.Text;
-            functionLabels.Add(fName, new FunctionDeclaration(node));
+            functionLabels.Add(fName, node);
 
             types.OpenScope();
 
@@ -153,53 +153,6 @@ namespace DLM.Compiler
             Add(lbl, type.DeclaredLabel);
         }
 
-        private class FunctionDeclaration
-        {
-            private string name;
-            private Label label;
-            private Dictionary<string, Parameter> parametersDic;
-            private Parameter[] parametersArr;
-
-            public string Name => name;
-            public Label Label => label;
-            public Parameter this[int number] => parametersArr[number];
-            public Parameter this[string name] => parametersDic[name];
-
-            public FunctionDeclaration(AFunctionDeclarationStatement functionDeclaration)
-            {
-                name = functionDeclaration.Identifier.Text;
-                label = functionDeclaration.Type.DeclaredLabel;
-                parametersDic = new Dictionary<string, Parameter>();
-                for (int i = 0; i < functionDeclaration.Parameters.Count; i++)
-                {
-                    var p = new Parameter(functionDeclaration.Parameters[i], i + 1);
-
-                    parametersDic.Add(p.Name, p);
-
-                    parametersArr = new Parameter[functionDeclaration.Parameters.Count];
-                    parametersArr[i] = p;
-                }
-            }
-
-            public class Parameter
-            {
-                private string name;
-                private int number;
-                private Label label;
-
-                public string Name { get { return name; } }
-                public int Number { get { return number; } }
-                public Label Label { get { return label; } }
-
-                public Parameter(PFunctionParameter parameter, int number)
-                {
-                    name = parameter.Identifier.Text;
-                    this.number = number;
-                    label = parameter.Type.DeclaredLabel;
-                }
-            }
-        }
-
         private class ExpressionLabeler : ReturnAnalysisAdapter<Label>
         {
             private LabelInferer owner;
@@ -262,13 +215,13 @@ namespace DLM.Compiler
             {
                 var fcName = node.Function.Text;
                 var hasCallerAuthority = node.Authorities.Count > 0;
-                FunctionDeclaration funcDecl;
+                AFunctionDeclarationStatement funcDecl;
                 Label fcLabel;
 
                 if (owner.functionLabels.TryGetValue(fcName, out funcDecl))
                 {
                     checkArgumentLabels(node.Arguments, funcDecl);
-                    fcLabel = getExplicitLabel(funcDecl.Label, node.Arguments.Select(x => Visit(x)).ToList(), funcDecl);
+                    fcLabel = getExplicitLabel(funcDecl.Type.DeclaredLabel, node.Arguments.Select(x => Visit(x)).ToList(), funcDecl);
                 }
                 else
                 {
@@ -305,29 +258,29 @@ namespace DLM.Compiler
             protected override Label HandleAParenthesisExpression(AParenthesisExpression node) => Visit(node.Expression);
             protected override Label HandleAPlusExpression(APlusExpression node) => Visit(node.Left) + Visit(node.Right);
 
-            private void checkArgumentLabels(Production.NodeList<PExpression> arguments, FunctionDeclaration functionDeclaration)
+            private void checkArgumentLabels(Production.NodeList<PExpression> arguments, AFunctionDeclarationStatement functionDeclaration)
             {
                 for (int i = 0; i < arguments.Count; i++)
                 {
                     var argLabel = Visit(arguments[i]);
-                    var paramLabel = functionDeclaration[i].Label;
+                    var paramLabel = functionDeclaration.Parameters[i].Type.DeclaredLabel;
                     if (paramLabel is PolicyLabel)
                         if (!(paramLabel <= argLabel))
                             errorManager.Register(arguments[i], $"Argument label is less restrictive than parameter label: {argLabel} \u228f {paramLabel}");
                 }
             }
 
-            private Label getExplicitLabel(ConstantLabel label, List<Label> argumentLabels, FunctionDeclaration functionDeclaration)
+            private Label getExplicitLabel(ConstantLabel label, List<Label> argumentLabels, AFunctionDeclarationStatement functionDeclaration)
             {
                 int number = functionDeclaration[label.Name].Number;
                 return argumentLabels[number-1];
             }
 
-            private Label getExplicitLabel(JoinLabel label, List<Label> argumentLabels, FunctionDeclaration functionDeclaration)
+            private Label getExplicitLabel(JoinLabel label, List<Label> argumentLabels, AFunctionDeclarationStatement functionDeclaration)
                 => getExplicitLabel((dynamic)label.Label1, argumentLabels, functionDeclaration)
                    + getExplicitLabel((dynamic)label.Label2, argumentLabels, functionDeclaration);
 
-            private Label getExplicitLabel(Label label, List<Label> argumentLabels, FunctionDeclaration functionDeclaration) => label;
+            private Label getExplicitLabel(Label label, List<Label> argumentLabels, AFunctionDeclarationStatement functionDeclaration) => label;
 
             private void checkAuthority(Production.NodeList<PPrincipal> authorities)
             {
