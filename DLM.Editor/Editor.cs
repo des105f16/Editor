65cea8f6ca92612f3cf55de231e59a94c2e9c49d
deaddog <mikkelslsl@gmail.com>
2016-03-30 20:33:51 +0200

    Better messaging on inference result.

--- DLM.Editor/Editor.cs
+++ DLM.Editor/Editor.cs
@@ -2,6 +2,7 @@
 using DLM.Inference;
 using SablePP.Tools.Editor;
 using System;
+using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
 
@@ -9,6 +10,12 @@ namespace DLM.Editor
 {
     public partial class Editor : SablePP.Tools.Editor.EditorForm
     {
+        private Font titleFont = new Font("Segoe UI", 10f, FontStyle.Regular);
+        private Font titleStateFont = new Font("Segoe UI", 10.5f, FontStyle.Bold);
+        private Font titleVariableFont = new Font("Consolas", 12f, FontStyle.Bold);
+
+        private Font infoFont = new Font("Segoe UI Symbol", 10.5f, FontStyle.Regular);
+
         public Editor()
         {
             InitializeComponent();
@@ -67,7 +74,7 @@ namespace DLM.Editor
 
             base.OnFileClosed(e);
         }
-        
+
         private void EditorForm_DragEnter(object sender, DragEventArgs e)
         {
             var files = GetDraggedFiles(e);
@@ -97,21 +104,92 @@ namespace DLM.Editor
 
             if (result.Item1.Succes)
                 foreach (var v in result.Item1.Variables)
-                    listBox1.Items.Add(getString(v));
+                    listBox1.Items.Add(v);
             else
-            {
-                listBox1.Items.Add("ERROR");
-                foreach (var v in result.Item1.ErrorConstraints)
-                {
-                    listBox1.Items.Add($"{v.Left} \u2291 {v.Right}");
-                    listBox1.Items.Add($"{v.Left.NoVariables.ToString()} \u2291 {v.Right.NoVariables.ToString()}");
-                }
-            }
+                foreach (var v in result.Item1.Constraints)
+                    listBox1.Items.Add(v);
         }
 
-        private string getString(VariableLabel label)
+        private void measureItem(object sender, MeasureItemEventArgs e)
         {
-            return label.Name + " \u2248 " + label.NoVariables.ToString();
+            var item = listBox1.Items[e.Index];
+
+            if (item is Constraint)
+                MeasureConstraint(item as Constraint, e);
+            if (item is VariableLabel)
+                MeasureVariable(item as VariableLabel, e);
+        }
+        private void drawItem(object sender, DrawItemEventArgs e)
+        {
+            var item = listBox1.Items[e.Index];
+
+            if (item is Constraint)
+                DrawConstraint(item as Constraint, e.State.HasFlag(DrawItemState.Focus), e);
+            if (item is VariableLabel)
+                DrawVariable(item as VariableLabel, e.State.HasFlag(DrawItemState.Focus), e);
+        }
+
+        private Color ok = Color.FromArgb(0xcaf0c2 + (0xff << 24));
+        private Color okFocus = Color.FromArgb(0x79d966 + (0xff << 24));
+        private Color error = Color.FromArgb(0xf5cccc + (0xff << 24));
+        private Color errorFocus = Color.FromArgb(0xe06666 + (0xff << 24));
+
+        private void MeasureConstraint(Constraint constraint, MeasureItemEventArgs e)
+        {
+            e.ItemHeight = 68;
+        }
+        private void DrawConstraint(Constraint constraint, bool selected, DrawItemEventArgs e)
+        {
+            var bounds = e.Bounds; bounds.Height -= 2;
+
+            var valid = constraint.Left <= constraint.Right;
+            using (var brush = new SolidBrush(valid ? (selected ? okFocus : ok) : (selected ? errorFocus : error)))
+                e.Graphics.FillRectangle(brush, bounds);
+
+            e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+
+            using (var brush = new SolidBrush(Color.FromArgb(204, Color.Black)))
+                e.Graphics.DrawString("Constraint:", titleFont, brush, bounds.X + 5, bounds.Y + 4);
+            e.Graphics.DrawString(valid ? "OK" : "Error", titleStateFont, Brushes.Black, bounds.X + 70, bounds.Y + 3);
+
+            var point = new PointF(bounds.X + 14, bounds.Y + 23);
+            
+            e.Graphics.DrawString($"{constraint.Left} ⊑ {constraint.Right}", infoFont, Brushes.Black, point);
+            point.Y += 15;
+            e.Graphics.DrawString($"{constraint.Left.NoVariables} ⊑ {constraint.Right.NoVariables}", infoFont, Brushes.Black, point);
+        }
+
+        private Color var = Color.FromArgb(0xcee8ef + (0xff << 24));
+        private Color varFocus = Color.FromArgb(0x84c5d8 + (0xff << 24));
+
+        private void MeasureVariable(VariableLabel label, MeasureItemEventArgs e)
+        {
+            e.ItemHeight = 53;
+        }
+        private void DrawVariable(VariableLabel label, bool selected, DrawItemEventArgs e)
+        {
+            var bounds = e.Bounds; bounds.Height -= 2;
+            
+            using (var brush = new SolidBrush(selected ? varFocus : var))
+                e.Graphics.FillRectangle(brush, bounds);
+
+            e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+
+            using (var brush = new SolidBrush(Color.FromArgb(204, Color.Black)))
+                e.Graphics.DrawString("Variable:", titleFont, brush, bounds.X + 5, bounds.Y + 4);
+            e.Graphics.DrawString(label.Name, titleVariableFont, Brushes.Black, bounds.X + 57, bounds.Y + 3);
+
+            var point = new PointF(bounds.X + 14, bounds.Y + 23);
+
+            e.Graphics.DrawString(label.NoVariables.ToString(), infoFont, Brushes.Black, point);
+        }
+
+        private float measureWidth(Graphics graphics, string text)
+        {
+            StringFormat format = new StringFormat();
+            format.SetMeasurableCharacterRanges(new CharacterRange[] { new CharacterRange(0, text.Length) });
+            var rectangle = graphics.MeasureCharacterRanges(text, infoFont, new RectangleF(0, 0, 1000, 1000), format)[0].GetBounds(graphics);
+            return rectangle.Width;
         }
     }
 }
