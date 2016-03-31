289dbeb4050b39c63c872438a86ce38887f99900
deaddog <mikkelslsl@gmail.com>
2016-03-31 09:28:47 +0200

    Created a new Constraint subtype that contains the Node that is the origin of the constraint.

---
+++ DLM.Compiler/NodeConstraint.cs
@@ -0,0 +1,22 @@
+﻿using DLM.Inference;
+using SablePP.Tools.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLM.Compiler
+{
+    public class NodeConstraint : Constraint
+    {
+        private Node origin;
+
+        public NodeConstraint(Label left, Label right, Node origin) : base(left, right)
+        {
+            this.origin = origin;
+        }
+
+        public Node Origin => origin;
+    }
+}
