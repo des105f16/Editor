d340e27712c7e72403163594abca28b280ecc424
deaddog <mikkelslsl@gmail.com>
2016-04-05 08:26:32 +0200

    Created a class for handling file events/actions.

---
+++ DLM.Wpf/EditorFile.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Input;
+
+namespace DLM.Wpf
+{
+    public class EditorFile
+    {
+        private string filepath;
+
+        public EditorFile(CommandBindingCollection bindings)
+        {
+            this.filepath = null;
+
+            bindings.Add(new CommandBinding(ApplicationCommands.New, null, enabled));
+
+            bindings.Add(new CommandBinding(ApplicationCommands.Open, null, enabled));
+            bindings.Add(new CommandBinding(CustomCommands.OpenRecent, null, disabled));
+
+            bindings.Add(new CommandBinding(ApplicationCommands.Save, null, fileLoaded_CanExecute));
+            bindings.Add(new CommandBinding(ApplicationCommands.SaveAs, null, fileLoaded_CanExecute));
+
+            bindings.Add(new CommandBinding(ApplicationCommands.Close, null, fileLoaded_CanExecute));
+        }
+
+        private void enabled(object sender, CanExecuteRoutedEventArgs e) => e.CanExecute = true;
+        private void disabled(object sender, CanExecuteRoutedEventArgs e) => e.CanExecute = false;
+        private void fileLoaded_CanExecute(object sender, CanExecuteRoutedEventArgs e) => e.CanExecute = filepath != null;
+    }
+}
--- DLM.Wpf/MainWindow.xaml.cs
+++ DLM.Wpf/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace DLM.Wpf
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        private EditorFile file;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -49,6 +51,8 @@ namespace DLM.Wpf
             };
 
             Loaded += (s, e) => codegrid.Children.Add(new WindowsFormsHost() { Child = codeTextBox });
+
+            file = new EditorFile(CommandBindings);
         }
     }
 }
