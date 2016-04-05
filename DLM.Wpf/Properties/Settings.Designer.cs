9ca04eeda4c253cc18f2f0707ec92ff52bfb469b
deaddog <mikkelslsl@gmail.com>
2016-04-05 11:13:08 +0200

    Created a Settings field for the file filter.

--- DLM.Wpf/Properties/Settings.Designer.cs
+++ DLM.Wpf/Properties/Settings.Designer.cs
@@ -61,7 +61,7 @@ namespace DLM.Wpf.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("{0} Files")]
         public string FileDescription {
             get {
                 return ((string)(this["FileDescription"]));
