8779d4bb825ca41f35924579e326a94f35168804
deaddog <mikkelslsl@gmail.com>
2016-04-05 07:03:41 +0200

    Created a static class with custom commands (Exit application and Open recent).

---
+++ DLM.Wpf/CustomCommands.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace DLM.Wpf
+{
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand Exit = new RoutedUICommand
+        (
+            "Exit",
+            "Exit",
+            typeof(CustomCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.F4, ModifierKeys.Alt)
+            }
+        );
+
+        public static readonly RoutedUICommand OpenRecent = new RoutedUICommand
+        (
+            "Open Recent",
+            "OpenRecent",
+            typeof(CustomCommands),
+            new InputGestureCollection()
+        );
+    }
+}
