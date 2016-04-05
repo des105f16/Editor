e5427f75b2f1a9c278dda5cb882145339d5d41e2
deaddog <mikkelslsl@gmail.com>
2016-04-05 10:46:16 +0200

    Added interface file event hookups.

--- DLM.Wpf/MainWindow.xaml.cs
+++ DLM.Wpf/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using MahApps.Metro.Controls;
 using SablePP.Tools.Editor;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.IO;
 using System.Windows.Forms.Integration;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DLM.Wpf
 {
@@ -23,6 +11,7 @@ namespace DLM.Wpf
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        private CodeTextBox codeTextBox;
         private EditorFile file;
 
         public MainWindow()
@@ -31,13 +20,14 @@ namespace DLM.Wpf
 
             var forecolor = System.Drawing.Color.FromArgb(220, 220, 220);
             var backcolor = System.Drawing.Color.FromArgb(30, 30, 30);
+            var disabledbackcolor = System.Drawing.Color.FromArgb(12, 12, 12);
             var linenumbers = System.Drawing.Color.FromArgb(36, 126, 175);
 
             var currentline = System.Drawing.Color.FromArgb(12, 12, 12);
 
             var font = new System.Drawing.Font("Consolas", 10f, System.Drawing.FontStyle.Regular);
 
-            var codeTextBox = new CodeTextBox()
+            this.codeTextBox = new CodeTextBox()
             {
                 Executer = new DLM.Compiler.CompilerExecuter(),
                 BackColor = backcolor,
@@ -47,7 +37,9 @@ namespace DLM.Wpf
                 CaretColor = forecolor,
                 ForeColor = forecolor,
                 CurrentLineColor = currentline,
-                Font = font
+                Font = font,
+                DisabledColor = disabledbackcolor,
+                Enabled = false
             };
 
             Loaded += (s, e) => codegrid.Children.Add(new WindowsFormsHost() { Child = codeTextBox });
@@ -57,6 +49,53 @@ namespace DLM.Wpf
                 FileExtension = ".ncif",
                 Title = "ncif"
             };
+
+            file.NewFileCreated += newFileCreated;
+            file.FileOpening += fileOpening;
+            file.FileOpened += fileOpened;
+            file.FileSaving += fileSaving;
+            file.FileClosed += fileClosed;
+        }
+        
+        private void newFileCreated(object sender, EventArgs e)
+        {
+            codeTextBox.Enabled = true;
+
+            codeTextBox.Text = "";
+            codeTextBox.Focus();
+            codeTextBox.SelectionLength = 0;
+            codeTextBox.SelectionStart = 0;
+
+            if (codeTextBox.Text == string.Empty)
+                codeTextBox.OnTextChangedDelayed(codeTextBox.Range);
+
+            codeTextBox.ClearUndo();
+        }
+        private void fileOpening(object sender, FileOpeningEventArgs e)
+        {
+            if (File.Exists(e.Filepath) && Path.GetExtension(e.Filepath) == file.FileExtension)
+                e.AllowFile = true;
+        }
+        private void fileOpened(object sender, FileOpenedEventArgs e)
+        {
+            codeTextBox.Enabled = true;
+
+            codeTextBox.Text = e.Content;
+            codeTextBox.ClearUndo();
+
+            codeTextBox.Focus();
+
+            if (codeTextBox.Text == string.Empty)
+                codeTextBox.OnTextChangedDelayed(codeTextBox.Range);
+        }
+        private void fileSaving(object sender, FileSavingEventArgs e)
+        {
+            e.Content = codeTextBox.Text;
+        }
+        private void fileClosed(object sender, EventArgs e)
+        {
+            codeTextBox.Enabled = false;
+            codeTextBox.Text = "";
         }
     }
 }
