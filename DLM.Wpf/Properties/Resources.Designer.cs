2354d3450e4898928b6c8cf3c80750d2cf862fb8
deaddog <mikkelslsl@gmail.com>
2016-04-07 09:17:05 +0200

    Removed the button bar and the error icon resource.

--- DLM.Wpf/Properties/Resources.Designer.cs
+++ DLM.Wpf/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace DLM.Wpf.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap error {
-            get {
-                object obj = ResourceManager.GetObject("error", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
