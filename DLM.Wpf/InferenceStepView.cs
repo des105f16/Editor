b6f7b06b0421c8b39452f1bf46575607008b7189
deaddog <mikkelslsl@gmail.com>
2016-04-12 11:03:22 +0200

    Created data templates for the various label types.

--- DLM.Wpf/InferenceStepView.cs
+++ DLM.Wpf/InferenceStepView.cs
@@ -19,10 +19,9 @@ namespace DLM.Wpf
 
         public string VariableName => step.Constraint.Left.ToString();
 
-        public string CurrentUpper => step.Left.NoVariables.ToString();
-        public string MeetWith => step.Right.ToString();
-        public string MeetWithNoVars => step.Right.NoVariables.ToString();
-        public string Result => step.Result.ToString();
+        public Label CurrentUpper => step.Left.NoVariables;
+        public Label MeetWith => step.Right;
+        public Label Result => step.Result;
 
         public string SourceType => step.Constraint.OriginType.ToString().ToLowerInvariant();
         public int Line => step.Constraint.MarkedOrigin.Line;
