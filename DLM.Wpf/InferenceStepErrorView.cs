233da036557ae802a491fc92151803599d104187
deaddog <mikkelslsl@gmail.com>
2016-04-19 16:13:05 +0200

    Removed implementation details from InferenceStepErrorView. They are contained in the default view.

--- DLM.Wpf/InferenceStepErrorView.cs
+++ DLM.Wpf/InferenceStepErrorView.cs
@@ -1,30 +1,12 @@
 ﻿using DLM.Compiler;
 using DLM.Inference;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DLM.Wpf
 {
-    public class InferenceStepErrorView
+    public class InferenceStepErrorView : InferenceStepView
     {
-        private ConstraintResolver<NodeConstraint>.Step step;
-
-        public InferenceStepErrorView(ConstraintResolver<NodeConstraint>.Step step)
+        public InferenceStepErrorView(ConstraintResolver<NodeConstraint>.Step step) : base(step)
         {
-            this.step = step;
         }
-
-        public string VariableName => step.Constraint.Left.ToString();
-
-        public Label CurrentUpper => step.Left.NoVariables;
-        public Label MeetWith => step.Right;
-        public Label MeetWithNoVars => step.Right.NoVariables;
-        public Label Result => step.Result;
-
-        public string SourceType => step.Constraint.OriginType.ToString().ToLowerInvariant();
-        public int Line => step.Constraint.MarkedOrigin.Line;
     }
 }
